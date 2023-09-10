d9cb79757fbef56003fee5710d5dd98ecf64d792
krzysiekk22d <klas.club@gmail.com>
2023-09-10 19:21:53 +0200

    Add functions: adding, removing, refreshing data.

--- Blurry/ContentView.swift
+++ Blurry/ContentView.swift
@@ -8,27 +8,78 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State var text = ["text0", "text1", "text2", "text3"]
-    @State var showsheet = false
+    @State var text: Array<String> = []
+    @State var showSheet = false
+    @State var textItemTemp = ""
     var body: some View {
         NavigationView {
-            List((text), id: \.self) { i in
-                Text(i)
+            Group {
+                if text.count <= 1 {
+                    Text("No items")
+                } else {
+                    List {
+                        ForEach((1...text.count - 1), id: \.self) { i in
+                            Text(text[i])
+                                .contextMenu {
+                                    Button {
+                                        text.remove(at: i)
+                                    } label: {
+                                        Label("Delete", systemImage: "trash")
+                                    }
+
+                                }
+                        }
+                    }
+                }
             }
             .navigationTitle("Notes")
             .toolbar {
                 Button {
-                    showsheet.toggle()
+                    showSheet.toggle()
+                    // clear the temp
+                    textItemTemp = ""
                 } label: {
                     Image(systemName: "plus")
                 }
             }
-        }
-        .sheet(isPresented: $showsheet) {
-            NavigationView {
-                Text("TEST")
+            .onChange(of: text) { _ in
+                save()
+                load()
+            }
+            .onAppear {
+                save()
+                load()
+            }
+            .refreshable {
+                save()
+                load()
             }
         }
+        .sheet(isPresented: $showSheet) {
+            NavigationView {
+                List {
+                    TextField("item", text: $textItemTemp)
+                }
+                .navigationTitle("Add an item")
+                .toolbar {
+                    Button("add") {
+                        text.append(textItemTemp)
+                        showSheet.toggle()
+                    }
+                }
+            }
+        }
+    }
+    func save() -> Void {
+        let temp = text.joined(separator: "/[split]/")
+        let key = UserDefaults.standard
+        key.set(temp, forKey: "text")
+    }
+    func load() -> Void {
+        let key = UserDefaults.standard
+        let temp = key.string(forKey: "text") ?? ""
+        let tempArray = temp.components(separatedBy: "/[split]/")
+        text = tempArray
     }
 }
 
